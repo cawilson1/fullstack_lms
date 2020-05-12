7c59386ff8092a68d57b9ba501c5d572ed6cc834
corleyb2 <corleyb2@gmail.com>
2020-05-12 11:45:28 -0400

    created components with boilerplate language

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <p>Nav 1</p>
+      <p>Nav 2</p>
+      <p>Nav 3</p>
+    </>
+  );
+};
+
+export default Nav;
---
+++ src/components/PostInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostInput = () => {
+  return (
+    <>
+      <h3>Post Input Here</h3>
+      <input type="text"></input>
+    </>
+  );
+};
+
+export default PostInput;
