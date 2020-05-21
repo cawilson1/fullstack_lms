17457f0702bed5421147459063af004dfa959a41
corleyb2 <corleyb2@gmail.com>
2020-05-21 14:59:42 -0400

    extended createdAt and updatedAt timestamps into Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,18 @@ import UpdatePostContainer from "../containers/UpdatePostContainer";
 const Post = ({ post, boundUpdatePost, boundAttemptDeletePost }) => {
   const [isToggleUpdate, setIsToggleUpdate] = useState(false);
 
+  function adjustedDate(date) {
+    let mm = date.slice(6, 7);
+    let dd = date.slice(8, 10);
+    let yy = date.slice(0, 4);
+    let hr = (date.slice(11, 13) - 4) % 12;
+    let min = date.slice(14, 16);
+    if (hr === 0) {
+      hr = 12;
+    }
+    return mm + "/" + dd + "/" + yy + " at " + hr + ":" + min;
+  }
+
   return (
     <div>
       {isToggleUpdate ? (
@@ -16,7 +28,15 @@ const Post = ({ post, boundUpdatePost, boundAttemptDeletePost }) => {
       ) : (
         <div>
           <h4>Start of Post by {post.author}</h4>
+          <p>Created: {adjustedDate(post.createdAt)}</p>
+          {post.createdAt == post.updatedAt ? (
+            <></>
+          ) : (
+            <p>Updated: {adjustedDate(post.updatedAt)}</p>
+          )}
+
           <p>{post.data}</p>
+
           <button onClick={() => boundAttemptDeletePost(post.id)}>
             Delete Post
           </button>
