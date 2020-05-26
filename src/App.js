27f2ff109617c7f737985a88b249658ec9be160e
corleyb2 <corleyb2@gmail.com>
2020-05-26 09:28:55 -0400

    safety commit before testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import { Auth } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import axios from "axios";
 
 import Header from "./components/Header";
 import Post from "./components/Post";
@@ -16,21 +17,60 @@ import NavContainer from "./containers/NavContainer";
 import Landing from "./components/Landing";
 
 function App() {
+  const [isProfile, setIsProfile] = useState(false);
+
+  useEffect(() => {
+    const checkProfileExists = async () => {
+      let userInfo = await Auth.currentUserInfo();
+      let userName = await userInfo.username;
+      console.log("meow", userName);
+      let checkDb = await axios({
+        method: "get",
+        url: `https://s9alxvtcob.execute-api.us-east-1.amazonaws.com/dev/user?username=${userName}`,
+      });
+      if (checkDb) {
+        console.log("true", true);
+        return setIsProfile(true);
+      } else {
+        console.log("false", false);
+        return setIsProfile(false);
+      }
+    };
+    checkProfileExists();
+  }, []);
+
   return (
     <div className="App">
       <NavContainer />
       <div style={styles.universal}>
         <Header />
-        <Router>
-          <UserProfileContainer path="profile" />
+        {isProfile ? (
+          <Router>
+            <UserProfileContainer
+              path="profile"
+              isProfile={isProfile}
+              setisProfile={setIsProfile}
+            />
+            <UserProfileInputContainer path="create_profile" />
+            <Landing path="/" />
+            <ResourcesListContainer path="resource_list" />
+            <ResourceInputContainer path="resource_input" />
+            <PostsListContainer path="post_list" />
+            <Post path="post" />
+            <PostInputContainer path="post_input" />
+          </Router>
+        ) : (
           <UserProfileInputContainer path="create_profile" />
+        )}
+        {/* <Router>
+          <UserProfileContainer path="profile" />
           <Landing path="/" />
           <ResourcesListContainer path="resource_list" />
           <ResourceInputContainer path="resource_input" />
           <PostsListContainer path="post_list" />
           <Post path="post" />
           <PostInputContainer path="post_input" />
-        </Router>
+        </Router> */}
       </div>
     </div>
   );
